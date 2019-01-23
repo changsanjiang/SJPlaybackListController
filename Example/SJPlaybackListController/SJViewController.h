aa098c2c65c09677d2cf47de1321af5eca34e62f
SanJiang <changsanjiang@gmail.com>
2019-01-23 18:49:00 +0800

    Initial commit

---
+++ Example/SJPlaybackListController/SJViewController.h
@@ -0,0 +1,13 @@
+//
+//  SJViewController.h
+//  SJPlaybackListController
+//
+//  Created by changsanjiang@gmail.com on 01/23/2019.
+//  Copyright (c) 2019 changsanjiang@gmail.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface SJViewController : UIViewController
+
+@end
---
+++ Example/SJPlaybackListController/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SJPlaybackListController
+//
+//  Created by changsanjiang@gmail.com on 01/23/2019.
+//  Copyright (c) 2019 changsanjiang@gmail.com. All rights reserved.
+//
+
+@import UIKit;
+#import "SJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SJAppDelegate class]));
+    }
+}
